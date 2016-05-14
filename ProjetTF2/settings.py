95a7a1cbce5442d6729d8bb51972941fbdc78d48
mioufoule <alexcantan@gmail.com>
2016-05-14 14:45:54 +0200

    modif list forums + djangobootstrap3

--- ProjetTF2/settings.py
+++ ProjetTF2/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'bootstrap3',
     'siteTF2',
 ]
 
@@ -84,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'tf2bd',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'alex',
+        'USER': 'postgre',
         'PASSWORD': 'lol147',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
